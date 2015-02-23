1e3850e4696c9f6e41c0262cf74e2ab7c71be9b5
geziogas <gs.ziogas@gmail.com>
2015-02-23 04:35:45 +0200

    Hmiteles Peer_sort
    Xwrizei ta elements se elem/Threads partitions kai kanei sorting ta
    partitions kai ta ektypwnei. Den exw kanei to merging akoma.

--- Peer_sort.c
+++ Peer_sort.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define NUM_ELEMENTS 10
+#include <pthread.h>
+
+#define NUM_ELEMENTS 20
+#define NUM_THREADS 4
+
+pthread_mutex_t mutex;
+struct thread_data{
+	double *A;
+	int left;
+	int right;
+	//int num_thr;
+};
 
 int partition(double a[], int left, int right){
 
@@ -29,17 +40,56 @@ int partition(double a[], int left, int right){
 
 	return j;
 }
+void *helpQuicksort(void *arg);
+void quicksort(double a[], int left, int right);
+void pquicksort(double a[], int size, int num_thr){
+
+	void *status;
+	pthread_t threads[NUM_THREADS];
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+	pthread_mutex_init(&mutex, NULL);
+	int t;
+	struct thread_data mydata[NUM_THREADS];
+	
+	//Size of elements for each thread
+	int chunk = size/num_thr;	
+
+	//Set left and right for all threads
+	for (t=0; t<NUM_THREADS; t++){
+		mydata[t].A = a;
+		mydata[t].left = t*chunk;
+		mydata[t].right = (t+1)*chunk - 1;
+      	pthread_create(&threads[t], &attr, helpQuicksort, (void *)&mydata[t]);
+   	}
+
+   	for (t=0; t<NUM_THREADS; t++){
+      pthread_join(threads[t], &status);
+   	}
+
+	pthread_attr_destroy(&attr);	
+}
 
 void quicksort(double a[], int left, int right){
 
 	int pivot;
-
+	
 	if( left < right ){
 		pivot = partition( a, left, right);
 		quicksort( a, left, pivot-1);
 		quicksort( a, pivot+1, right);
 	}
 
+}
+
+void *helpQuicksort(void *arg){
+
+	int pivot;
+	struct thread_data *mydata;
+	mydata = (struct thread_data *) arg;
+	// printf("%d\n", mydata-);
+	quicksort(mydata->A,mydata->left,mydata->right);
 }
 
 int main(int argc, char *argv[]) {
@@ -55,7 +105,7 @@ int main(int argc, char *argv[]) {
 	double scaleLimit = 100.0;
 	double divisor = (double)RAND_MAX/scaleLimit;
 
-	printf("\n\nUnsorted: ");
+	printf("\n\nUnsorted: \n");
 	for (i=0;i<NUM_ELEMENTS;i++){
 		A[i] = rand()/divisor;
 		// Variemai na psaksw na kanw tin drand48/srand48 na doulepsei sta windows
@@ -63,12 +113,12 @@ int main(int argc, char *argv[]) {
 		printf(" %f ",A[i]);
 	}
 
-	quicksort(A,0,NUM_ELEMENTS-1);
+	pquicksort(A,NUM_ELEMENTS,NUM_THREADS);
 
-	printf("\n\nSorted: ");
+	printf("\n\nSorted: \n");
 	for (i=0;i<NUM_ELEMENTS;i++){
 			printf(" %f ",A[i]);
 		}
-
+	printf("\n");
 	return 0;
 }
